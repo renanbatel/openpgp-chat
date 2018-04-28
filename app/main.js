600b5d076d8cdfee748b90501cfe4fa2d696c7b1
renanbatel <renanbatel@gmail.com>
2018-04-28 12:19:50 -0300

    first commit

---
+++ app/main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+require( 'electron-reload' )( __dirname );
+
+const electron    = require( 'electron' );
+const path        = require( 'path' );
+const url         = require( 'url' );
+
+const app           = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+
+app.on( 'ready', () => {
+
+  mainWindow = new BrowserWindow( {
+    width:  960,
+    height: 540
+  } );
+
+  mainWindow.loadURL( url.format( {
+    pathname: path.join( __dirname, 'index.html' ),
+    protocol: 'file',
+    slashes:  true
+  } ) );
+
+} );
