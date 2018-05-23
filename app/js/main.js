2ed396eef73192623f0f5130d22ae9671d5410cb
gromeroim <gabriel.romero@ykt.com.br>
2018-05-22 23:29:30 -0300

    Retorna usuário, função deslogar

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-require( './js/view/login' );
+require('./js/view/login');
 
 //Aqui se encontra as funções necessárias para interagir com o firebase
 var config = {
@@ -13,28 +13,53 @@ firebase.initializeApp(config);
 
 var btnCadastrar = document.getElementById('cadastrar');
 var btnLogin = document.getElementById('login');
+var btnLogout = null;
+const dbRefMensagem = firebase.database().ref().child('mensagens'); //cria referencia no bd
 
-btnCadastrar.addEventListener('click', function(){
+//Cadastrar usuário
+btnCadastrar.addEventListener('click', function () {
     var email = document.getElementById('signup_email').value;
-    var senha =  document.getElementById('signup_senha').value;
-    firebase.auth().createUserWithEmailAndPassword(email, senha).then(function(){
-        alert('Usuario Criado')
-    }).catch(function(error){
-        if(error != null){
-            console.log("erro "+error);
+    var senha = document.getElementById('signup_senha').value;
+    firebase.auth().createUserWithEmailAndPassword(email, senha).then(function () {
+        alert('Usuario Criado');
+    }).catch(function (error) {
+        if (error != null) {
+            console.log("erro " + error);
             return;
         }
     });
 });
 
-btnLogin.addEventListener('click', function(){
+//Logar com email e senha
+btnLogin.addEventListener('click', function () {
     var email = document.getElementById('email').value;
-    var senha =  document.getElementById('senha').value;
-    firebase.auth().signInWithEmailAndPassword(email, senha).then(function(){
+    var senha = document.getElementById('senha').value;
+    firebase.auth().signInWithEmailAndPassword(email, senha).then(function () {
         console.log('logou')
-    }).catch(function(error){
-        if(error != null){
+    }).catch(function (error) {
+        if (error != null) {
             console.log('errou')
         }
     });
+});
+//verifica quando um usuário loga ou desloga
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log(user)
+        //colocar o botão de sair
+    } else {
+        console.log('sem usuário logado')
+        //tirar o botão sair
+    }
+});
+
+function retornaUsuario(){
+    if(firebase.auth().currentUser){
+        return true;
+    }
+    return false;
+}
+
+btnLogout.addEventListener('click', e =>{
+    firebase.auth().signOut();
 });
